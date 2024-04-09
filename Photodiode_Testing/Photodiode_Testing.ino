c5208af9925730760795b019fdeb054d44e6af7b
mayabartels <mayabartels01@gmail.com>
2024-04-09 16:12:43 -0700

    Create Photodiode_Testing.ino

---
+++ Photodiode_Testing/Photodiode_Testing.ino
@@ -0,0 +1,13 @@
+/* BIM 110 - UC Davis Senior Design */
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
