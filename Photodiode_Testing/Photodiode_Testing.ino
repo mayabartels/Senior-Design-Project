0828f8750d1b9e400a8f2f327b8dc6dd60398010
mayabartels <mayabartels01@gmail.com>
2024-04-10 13:16:49 -0700

    Update with code to get Voltage from analog read values

--- Photodiode_Testing/Photodiode_Testing.ino
+++ Photodiode_Testing/Photodiode_Testing.ino
@@ -1,13 +1,18 @@
 /* BIM 110 - UC Davis Senior Design */
 
+const int photodiodeRedPin = A0;
 
+int photodiodeRedValue = 0;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  pinMode(photodiodeRedValue, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
+  photodiodeRedValue = analogRead(photodiodeRedPin);
+  float voltagePhotodiodeRed = photodiodeRedValue * (5 / 1023);
+  Serial.println(photodiodeRedValue);
 }
