038397aaa5149b2fbbe33ad757f1a5a410061337
mayabartels <mayabartels01@gmail.com>
2024-04-15 15:56:04 -0700

    Add photodiode measurement code to main arduino code

--- Arduino_Main/Arduino_Main.ino
+++ Arduino_Main/Arduino_Main.ino
@@ -1,21 +1,26 @@
-/* BIM 110 - UC Davis Senior Design 
-  authors: Deesha Patel, Maya Bartels */
+/* BIM 110 - UC Davis Senior Design */
 
 //initialize variables
 const int redPin = 11;
 const int IRPin = 10;
 const int buttonPin = 4;
 
-int buttonState = 0;
+int photodiodePinRed = A1; //red photodiode
+int photodiodePinIR = A2; //IR photodiode
+
+int buttonState = 0; //set initial button pressed state to false
 
 void startMeasurement(){
-  //redLED light
+  //red LED light
   digitalWrite(redPin, HIGH);
   delay(1000);
   digitalWrite(redPin, LOW);
   delay(1000);
 
-  //photodiode red
+  //read voltage for red photodiode
+  photodiodeRedValue = analogRead(photodiodePinRed);
+  float voltagePhotodiodeRed = photodiodeRedValue * (3.3 / 1023); // 3.3 V / 1023 analog units
+  Serial.println("Red Voltage: " + voltagePhotodiodeRed);
   
   //IRLED light
   digitalWrite(IRPin, HIGH);
@@ -23,21 +28,33 @@ void startMeasurement(){
   digitalWrite(IRPin, LOW);
   delay (1000);
 
-  //photodiode IR
+  //read voltage for IR photodiode
+  photodiodeIRValue = analogRead(photodiodePinIR);
+  float voltagePhotodiodeIR = photodiodeIRValue * (3.3 / 1023); // 3.3 V / 1023 analog units
+  Serial.println("IR Votage: " + voltagePhotodiodeIR);
 }
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin();
+
+  //initialize LEDs
   pinMode(redPin, OUTPUT);
   pinMode(IRPin, OUTPUT);
+
+  //initialize Button
   pinMode(buttonPin, INPUT);
+
+  //initialize Photodiodes
+  pinMode(photodiodePinRed, INPUT);
+  pinMode(photodiodePinIR, INPUT);
 }
 
 void loop() {
   //button on or off
   buttonState = digitalRead(buttonPin);
 
-  //detect if button has been pressed
+  //detect if button has been pressed, then start the pH measurement
   if (buttonState == HIGH) {
     startMeasurement();
   } else {
